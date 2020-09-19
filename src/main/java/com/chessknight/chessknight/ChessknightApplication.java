1b2d01097eb6a95394a6f04335051568b8206816
Spyros Angelopoulos <angelopoulos.spyr@gmail.com>
2020-09-19 17:05:55 +0300

    Initial commit

---
+++ src/main/java/com/chessknight/chessknight/ChessknightApplication.java
@@ -0,0 +1,13 @@
+package com.chessknight.chessknight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessknightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessknightApplication.class, args);
+    }
+
+}
